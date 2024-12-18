9136310ad2d7b13048ebf5c6c010a5123b5a289d
gabrielmoraiis <bielmorais2004@gmail.com>
2024-12-18 13:36:35 -0300

    feat: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Navbar from "./_components/navbar";
 
 const Home = async () => {
-  const {userId} = await auth();
-  if(!userId) {
-    redirect("/login")
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
   }
-  return (
-    <div className="h-full flex items-center justify-center">
-      
-    <UserButton showName/>
-    </div>
-  );
+  return <Navbar />;
 };
 
 export default Home;
